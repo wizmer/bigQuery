dcc32af1511ca4d3ffcc51b5a7318d4c81a0f815
KKostya <kkanishev@gmail.com>
2015-06-22 17:48:11 +0200

    update

--- mcmc/matrixRead.py
+++ mcmc/matrixRead.py
@@ -1,48 +1,11 @@
-#
-# "Theoretical" binning (the one we will look the spectrum for)
-#  is bootstrapped from R-vs-beta curves for D and P 
-#
-def make_beta_bins(beta):
-    bbins = []
-    for i in range(10):
-        bbins.append(beta)
-        beta = beta_from_R(R_from_beta(beta,md),mp)
-    return bbins
+frame = pd.DataFrame.from_csv("datasets/R_resolution.csv")
+frame = frame.divide(frame.sum(axis=1),axis=0)
+rgdtMeasured  = np.array(frame.columns.astype(float))
+rgdtTheoretic = np.array(frame.index.astype(float))
+rgdtF = frame.values[:-1,:-1]
 
-bbins = make_beta_bins(0.5)
-bbins += make_beta_bins((bbins[1]+bbins[0])/2)
-bbins = sorted(bbins)
-
-mid1,mid2 = (bbins[1]+bbins[0])/2,(bbins[2]+bbins[1])/2
-bbins += make_beta_bins(mid1)
-bbins += make_beta_bins(mid2)
-bbins = np.array(sorted(bbins))
-
-betaTheoretic, rgdtTheoretic = np.array([bbins, R_from_beta(bbins, mp)])
-
-
-
-#
-# Measured values binnings are not very fancy
-#
-betaMeasured = 1/np.linspace(0.5,2,28)
-betaMeasured.sort()
-
-rgdtMeasured = np.logspace(-5.0 / 19, 1, 25)
-
-# This reads matrices
-
-def get_matrix(filename, binsT, binsM):
-    rres = pd.DataFrame.from_csv(filename)
-    rres = rres.divide( rres.sum(axis=1), axis=0)
-    rres.columns = binsM
-    rres.index = list(rres.index[:-1]) + [binsT[-1]]
-    rres.index = rres.index.astype(float)
-    rres = rres.reindex(binsT).fillna(0.0)
-    return rres.values[:-1,:-1]
-
-rgdtF = get_matrix("datasets/R_resolution.csv", rgdtTheoretic, rgdtMeasured)
-model.set_rigidity_resolution(rgdtF)
-
-betaF = get_matrix("datasets/B_resolution.csv", betaTheoretic, betaMeasured)
-model.set_beta_resolution(betaF)
+frame = pd.DataFrame.from_csv("datasets/B_resolution.csv")
+frame = frame.divide(frame.sum(axis=1),axis=0)
+betaMeasured  = np.array(frame.columns.astype(float))
+betaTheoretic = np.array(frame.index.astype(float))
+betaF = frame.values[:-1,:-1]
