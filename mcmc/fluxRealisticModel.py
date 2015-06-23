7f68f1201c93a84d50d0f90a0ee0791ada839159
KKostya <kkanishev@gmail.com>
2015-06-23 16:25:56 +0200

    make_mock_observation

--- mcmc/fluxRealisticModel.py
+++ mcmc/fluxRealisticModel.py
@@ -3,28 +3,42 @@ from pd_model import *
 import pandas as pd
 import numpy as np
 
-if __name__ == "__main__":
-    
+
+def load_model(filenameR, filenameB):
     frame = pd.DataFrame.from_csv("datasets/R_resolution.csv")
     frame = frame.divide(frame.sum(axis=1),axis=0)
     rgdtMeasured  = np.array(frame.columns.astype(float))
     rgdtTheoretic = np.array(frame.index.astype(float))
     rgdtF = frame.values[:-1,:-1]
-
+    
     frame = pd.DataFrame.from_csv("datasets/B_resolution.csv")
     frame = frame.divide(frame.sum(axis=1),axis=0)
     betaMeasured  = np.array(frame.columns.astype(float))
     betaTheoretic = np.array(frame.index.astype(float))
     betaF = frame.values[:-1,:-1]
-   
+    
     model = PDModel(
-        betaBinsTheoretic=betaTheoretic,
-        betaBinsMeasured=betaMeasured ,
-        rgdtBinsTheoretic=rgdtTheoretic, 
-        rgdtBinsMeasured=rgdtMeasured 
-    )
+            betaBinsTheoretic=betaTheoretic,
+            betaBinsMeasured=betaMeasured ,
+            rgdtBinsTheoretic=rgdtTheoretic, 
+            rgdtBinsMeasured=rgdtMeasured 
+            )
     model.set_rigidity_resolution(rgdtF)
     model.set_beta_resolution(betaF)
+    return model
+
+def make_mock_observation(fluxP, fluxD, write_file=None):
+    model = load_model("datasets/R_resolution.csv", "datasets/B_resolution.csv")
+    r = model(fluxP,fluxD)
+    
+    if not write_file is None:
+        np.savetxt(write_file,r)
+    
+    return r
+
+if __name__ == "__main__":
+   
+    model = load_model("datasets/R_resolution.csv", "datasets/B_resolution.csv")
 
     observed = np.loadtxt("datasets/observed_mock_equal.txt")
 
