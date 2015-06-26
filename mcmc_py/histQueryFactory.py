f5d2cc399eb2cfc115efdeb68d1bb3410f79a5e7
KKostya <kkanishev@gmail.com>
2015-06-26 23:52:24 +0200

    move pyhton code

---
+++ mcmc_py/histQueryFactory.py
@@ -0,0 +1,70 @@
+class HistQueryFactory(object):
+    """
+    h = HistQueryFactory()
+    h.add_variable("R",0,10,40)
+    h.add_variable("BetaTOF",0,1,10)
+    h.add_condition("Rcutoff < R")
+    h.add_condition("Latitude > 0.8")
+    h.add_condition("Latitude < 0.9")
+    print str(h)
+    """    
+    def __init__(self, table="full_test.full_test", bins='mid'):
+        self.table = table
+        self.bins = bins
+        self.variables = {}
+        self.binnames = {}
+        self.varorder = []
+        self.extra_conditions = []
+        self.aggregators = [] 
+        self.join = None 
+    
+    def add_variable(self, var, min=0, max=1, nbins=100, binname=None):
+        self.variables[var] = [min, max, nbins]
+        self.binnames[var] = var+"_BIN" if binname is None else binname
+        self.varorder.append(var)
+    
+    def add_condition(self, condition):
+        self.extra_conditions.append(condition)
+        
+    def add_aggregator(self, aggregator):
+        self.aggregators.append(aggregator)
+        
+    def add_join(self, join, on, alias):
+        self.join = (str(join), on, alias)
+        
+    def get_variables(self):
+        return [self.binnames[v] for v in self.varorder]
+    
+    def __str__(self):
+        select, where = [], []
+        binexpr = "STRING({mi:f} + ({rng:f})*FLOOR({N:f}*(({var})-({mi:f}))/{rng:f})/{N:f}) as {bname}"
+        for v in self.varorder:
+            mi,ma,nb = self.variables[v]
+            bname = self.binnames[v]
+            select.append(binexpr.format(var=v, mi=mi, N=nb, rng=ma-mi, bname=bname))
+            where.append("{0} >= {1} AND {0} < {2}".format(v,mi,ma))
+            
+        if not self.aggregators:
+            select += ["COUNT(1) as count"]
+        else:
+            select += self.aggregators 
+        
+        where += self.extra_conditions
+        
+        if self.join is not None:
+            self.table += " JOIN EACH ({0}) AS {1} ON {2}".format(*self.join)
+        
+        query = \
+        "SELECT\n   {select}\n"\
+        "FROM\n   {from}\n"\
+        "WHERE\n   {where}\n"\
+        "GROUP BY {bins}\n"\
+        "ORDER BY {bins}\n"
+        
+        dic = {
+            "select": ",\n   ".join(select),
+            "from": self.table,
+            "where":" AND\n   ".join(where),
+            "bins":",".join(self.get_variables())
+        }
+        return  query.format(**dic)    
